d987462dd99e2eb2fa86abb19e2bdec7fe849025
Tim Kuehn <tkuehn@cmu.edu>
2013-05-30 18:31:47 -0700

    work in progress

--- point.rs
+++ point.rs
@@ -7,6 +7,8 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
+use num::Algebraic;
+
 #[deriving(Eq)]
 pub struct Point2D<T> {
     x: T,
@@ -18,15 +20,24 @@ pub fn Point2D<T:Copy>(x: T, y: T) -> Point2D<T> {
 }
 
 
-impl<T:Copy + Add<T,T>> Add<Point2D<T>, Point2D<T>> for Point2D<T> {
+impl<T:Copy + Num> Add<Point2D<T>, Point2D<T>> for Point2D<T> {
     fn add(&self, other: &Point2D<T>) -> Point2D<T> {
         Point2D(self.x + other.x, self.y + other.y)
     }
 }
 
-impl<T:Copy + Sub<T,T>> Sub<Point2D<T>, Point2D<T>> for Point2D<T> {
+impl<T:Copy + Num> Sub<Point2D<T>, Point2D<T>> for Point2D<T> {
     fn sub(&self, other: &Point2D<T>) -> Point2D<T> {
         Point2D(self.x - other.x, self.y - other.y)
     }
 }
 
+impl<T:Num> Point2D<T> {
+    fn norm(&self) -> float {
+        Algebraic::sqrt((self.x * self.x) + (self.y * self.y))
+    }
+
+    fn dist(&self, other: &Point2D<T>) -> float {
+        (other - self).norm()
+    }
+}
